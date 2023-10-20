3fb05c4261e6d694797c4be540e0eef1f57ae45f
JSA-Code <jsafortech+github@gmail.com>
2023-10-19 20:57:36 -0700

    install plaiceholder for creating low quality image placeholders (LQIP) add helper function getBase64() which will grab the base64 from each image url by mapping and using Promise.all, then assigning blurred data url into images array add imgContainer.tsx to contain the image, style it, and use a placeholder update Gallery.tsx to use blurred placeholder

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,15 +1,21 @@
 import fetchImages from "@/lib/fetchImages";
 import type { ImagesResults } from "@/models/Images";
+import ImgContainer from "./ImgContainer";
+import addBlurredDataUrls from "@/lib/getBase64";
 
 export default async function Gallery() {
   const url = "https://api.pexels.com/v1/curated";
   const images: ImagesResults | undefined = await fetchImages(url);
+
   if (!images)
     return <h2 className="m-4 text-2xl font-bold">No images found.</h2>;
+
+  const photosWithBlur = await addBlurredDataUrls(images);
+
   return (
     <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
-      {images.photos.map((photo) => (
-        <div className="h-64 bg-gray-200 rounded-xl" key={photo.id}></div>
+      {photosWithBlur.map((photo) => (
+        <ImgContainer key={photo.id} photo={photo} />
       ))}
     </section>
   );
---
+++ src/components/ImgContainer.tsx
@@ -0,0 +1,22 @@
+import type { Photo } from "@/models/Images";
+import Image from "next/image";
+
+type Props = {
+  photo: Photo;
+};
+
+export default function ImgContainer({ photo }: Props) {
+  return (
+    <div className="h-64 bg-gray-200 rounded-xl relative overflow-hidden group">
+      <Image
+        className="object-cover hover:opacity-75 group-hover:scale-110 duration-100"
+        src={photo.src.large}
+        alt={photo.alt}
+        sizes="(min-width: 1620px) 276px, (min-width: 1320px) calc(8.57vw + 139px), (min-width: 1060px) 25vw, (min-width: 800px) calc(33.33vw - 14px), (min-width: 540px) calc(50vw - 15px), calc(100vw - 20px)"
+        placeholder="blur"
+        blurDataURL={photo.blurredDataUrl}
+        fill={true}
+      />
+    </div>
+  );
+}
